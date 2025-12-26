89a98be5fda6f96180e98560d2b15b839dece95f
Brainmoses254 <joinmoses254@gmail.com>
2025-12-26 15:17:16 +0300

    Set up Express server with basic route

--- Index.js
+++ Index.js
@@ -1,1 +1,13 @@
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express (ESM)!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
